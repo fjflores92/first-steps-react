ac402d115d41622e6075fc90be2bb19cb88e2679
Francisco Flores <fflores.ic.ipn@hotmail.com>
2020-08-14 23:23:43 -0500

    State - Events

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,13 +1,29 @@
 import React from 'react';
+import { render } from 'react-dom';
 
-const Button = props => {
-    const { text } = props;
-    return(
-        <div>
-            <button type="button">{props.text}</button>
-            <p>{text}</p>
-        </div>
-    );
+class Button extends React.Component {
+    
+    state = {
+        count: 0,
+    }
+
+    clickHandler = () => {
+        this.setState({
+            count: this.state.count + 1
+        });
+    }
+
+    render() {
+        const { count } = this.state;
+        return(
+            <div>
+                <h1>Cervezas: {count}</h1>
+                <button type="button" onClick={this.clickHandler}>
+                    Más Cerveza
+                </button>
+            </div>
+        )
+    };
 };
 
 export default Button;
